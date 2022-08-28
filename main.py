53c7a8080b822b4e347e7c19d5e0fdacd28ff23e
sadadsafsgfdgd <112297578+sadadsafsgfdgd@users.noreply.github.com>
2022-08-28 16:51:00 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ import os
 def get_color():
     # 获取随机颜色
     get_colors = lambda n: list(map(lambda i: "#" + "%06x" % random.randint(0, 0xFFFFFF), range(n)))
-    color_list = Red(100)
+    color_list = get_colors(100)
     return random.choice(color_list)
 
 
